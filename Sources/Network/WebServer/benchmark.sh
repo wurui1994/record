76d52f29815152b3320ba439f14d7b78f9e6cbcb
wurui2 <wurui2@kingsoft.com>
2019-06-06 14:37:27 +0800

    update webserver example.

--- Sources/Network/WebServer/benchmark.sh
+++ Sources/Network/WebServer/benchmark.sh
@@ -11,3 +11,9 @@ python test.py
 # `apib` tool: https://github.com/apigee/apib 
 apib -c1 -d10 http://127.0.0.1:8000/
 apib -c100 -d10 http://127.0.0.1:8000/
+
+# `wrk` tool: https://github.com/wg/wrk
+wrk http://127.0.0.1:8000/
+
+# `bombardier` tool: https://github.com/codesenberg/bombardier
+bombardier http://127.0.0.1:8000/
