d3703d6b355152ef6c88b6970c9041c2a8cb421b
wurui <1341531859@qq.com>
2020-11-25 21:07:27 +0800

    update build tcc for vs2019

--- Build/VCPKG/VCPKG_tcc/build.bat
+++ Build/VCPKG/VCPKG_tcc/build.bat
@@ -1,14 +1,14 @@
 @echo off
 REM git.exe clone http://repo.or.cz/tinycc.git 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat""
-mkdir tcc-0.9.27
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+mkdir tcc-binary
 pushd .
 cd tinycc\win32
-REM @call build-tcc.bat -c cl -t 32 -i ..\..\tcc-0.9.27 -b ..\..\tcc-0.9.27\tools
-call build-tcc.bat -c cl -t 32 -i ..\..\tcc-0.9.27
+REM @call build-tcc.bat -c cl -t 32 -i ..\..\tcc-binary -b ..\..\tcc-binary\tools
+call build-tcc.bat -c cl -t 32 -i ..\..\tcc-binary
 call build-tcc.bat -clean
 popd
-cd tcc-0.9.27
+cd tcc-binary
 tcc -Ilibtcc -L. -ltcc -run examples\libtcc_test.c
 pause
 REM cls
