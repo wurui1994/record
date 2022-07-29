8d956754544251c4873cc7cdf4cda6d7faa7fb31
rui.wu <rui.wu@qingteng.cn>
2022-07-29 11:17:52 +0800

    update auto_free example

--- Sources/C++/CLang/auto_free.c
+++ Sources/C++/CLang/auto_free.c
@@ -1,20 +1,27 @@
 // gcc or clang
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-#define auto_free __attribute__ (( cleanup(free_ptr) ))
+#if __has_attribute(cleanup)
+#define auto_free __attribute__((cleanup(free_ptr)))
+#else
+#define auto_free
+#endif
 
-__attribute__((always_inline))
-inline void free_ptr(void* str) 
+void free_ptr(void *str)
 {
-    if( !str ) return;
-    free( *(void**)str );
+    if (!str)
+        return;
+    free(*(void **)str);
     printf("auto release\n");
 }
 
 int main()
 {
-    auto_free char* tmp_str = calloc(100,1);
+    auto_free char *tmp_str = calloc(100, 1);
+    strcpy(tmp_str, "hello");
+    printf("%s\n", tmp_str);
     return 0;
 }
