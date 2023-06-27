82a17858639cc5f4f1f7c912a05db3cc1802d9aa
wurui <1341531859@qq.com>
2023-06-27 16:42:51 +0800

    remove ununsed note

--- Python/Anaconda/notes.bat
+++
@@ -1,4 +0,0 @@
-set Path=D:\Anaconda3;D:\Anaconda3\Library\mingw-w64\bin;D:\Anaconda3\Library\usr\bin;D:\Anaconda3\Library\bin;D:\Anaconda3\Scripts;D:\Anaconda3\bin;
-
-REM D:\Anaconda3\Library\bin;D:\Anaconda3\Scripts;
-REM numpy need: D:\Anaconda3\Library\bin;
